5d5812225d4a15209b6ae03c4af761efabc22a53
LunzagaJa <73624891+LunzagaJa@users.noreply.github.com>
2025-05-24 11:41:46 +0800

    newest ver

--- VariableAndDataTypes/Program.cs
+++ VariableAndDataTypes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,19 +12,19 @@ namespace Episode1VariableAndDataType
 
         static void Main(string[] args)
         {
+
             #region -- Local Variables --
             int sampleNumber = 3;
             var sampleNumberImplicit = 3;
             dynamic sampleNumberDynamic = 5;
             Console.WriteLine("This is my sample: "+sampleNumber);
             #endregion
-            
+            #region --Class Variables--
+            // Instance variables
             var episode1 = new Episode1Class();
             Console.WriteLine(episode1.samplePublicInt);
             episode1.samplePublicInt = 250;
             Console.WriteLine(episode1.samplePublicInt);
-<<<<<<< HEAD
-=======
 
             //Class  or Static Variables
 
@@ -34,24 +35,24 @@ namespace Episode1VariableAndDataType
 
 
             #endregion
-            // new git version
             // delete this entire function
             // Parse and convert
             #region -- parse and convert --
             String textOne = "3";
             var intOne = int.Parse(textOne);
             var result = intOne * 5;
-            Console.WriteLine($" Parse  = Text one result: { result }");
+            Console.WriteLine($" Parse  = Text one result: {result}");
 
             var intConverted = Convert.ToInt32(textOne);
             result = intConverted * 6;
-            Console.WriteLine($" Convert = Text one result: { result }");
+            Console.WriteLine($" Convert = Text one result: {result}");
             #endregion
->>>>>>> 0fba3c0edf7f7d1c8a561d4114ce9aee2bf8060f
         }
     }
     public class Episode1Class
     {
+        public static int sampleStaticOrClassVariableInt = 3;
+
         public int samplePublicInt = 500;
         private int samperPrivateInt = 100;
 
