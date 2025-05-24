598196c7efb4e3d9a20acd44f9b231b3b83b2b72
LunzagaJa <73624891+LunzagaJa@users.noreply.github.com>
2025-05-24 11:21:12 +0800

    ako budoy

--- VariableAndDataTypes/Program.cs
+++ VariableAndDataTypes/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,47 +11,21 @@ namespace Episode1VariableAndDataType
 
         static void Main(string[] args)
         {
-
             #region -- Local Variables --
             int sampleNumber = 3;
             var sampleNumberImplicit = 3;
             dynamic sampleNumberDynamic = 5;
             Console.WriteLine("This is my sample: "+sampleNumber);
             #endregion
-            #region --Class Variables--
-            // Instance variables
+            
             var episode1 = new Episode1Class();
             Console.WriteLine(episode1.samplePublicInt);
             episode1.samplePublicInt = 250;
             Console.WriteLine(episode1.samplePublicInt);
-
-            //Class  or Static Variables
-
-            Console.WriteLine("*************");
-            Console.WriteLine(Episode1Class.sampleStaticOrClassVariableInt);
-            Episode1Class.sampleStaticOrClassVariableInt = 500;
-            Console.WriteLine(Episode1Class.sampleStaticOrClassVariableInt);
-
-
-            #endregion
-            // delete this entire function
-            // Parse and convert
-            #region -- parse and convert --
-            String textOne = "3";
-            var intOne = int.Parse(textOne);
-            var result = intOne * 5;
-            Console.WriteLine($" Parse  = Text one result: { result }");
-
-            var intConverted = Convert.ToInt32(textOne);
-            result = intConverted * 6;
-            Console.WriteLine($" Convert = Text one result: { result }");
-            #endregion
         }
     }
     public class Episode1Class
     {
-        public static int sampleStaticOrClassVariableInt = 3;
-
         public int samplePublicInt = 500;
         private int samperPrivateInt = 100;
 
